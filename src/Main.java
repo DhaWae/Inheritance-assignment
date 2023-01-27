d9c20d39ced68d4c4612805d9e84f8c6c8c9c8eb
Jesper <Jesperrudegran@gmail.com>
2023-01-27 09:36:59 +0100

    Added array of different objects

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Bicycle pettersBicycle = new Bicycle(2, 63, "Crescent", 10, 24);
-        System.out.println("Petter Bicycle: " + "\n" + pettersBicycle.printBicycleInfo());
+        //System.out.println("Petter Bicycle: " + "\n" + pettersBicycle.printBicycleInfo());
 
         Car pettersCar = new Car(4,1924,"Toyota",6,150,5,2,2009,"E98","R343",true);
-        System.out.println("Petter Car: " + "\n" + pettersCar.printCarInfo());
+        //System.out.println("Petter Car: " + "\n" + pettersCar.printCarInfo());
 
 
         Bus[] pettersBusBusiness;
@@ -21,5 +23,14 @@ public class Main {
         for (Bus bus: pettersBusBusiness) {
             System.out.println(bus.printBusInfo());
         }
+
+        List<Object> objects = new ArrayList<Object>();
+        objects.add(new Car(4,1924,"Toyota",6,150,5,2,2009,"E98","R343",true));
+        objects.add(new Bus(6,4000,"Mercedes",6,340,65,420,2000,"Diesel",2000));
+        objects.add(new Bicycle(2, 63, "Crescent", 10, 24));
+        Car car = (Car) objects.get(0);
+        System.out.println(car.printCarInfo());
+
+
     }
 }
